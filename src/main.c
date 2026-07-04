1386719b9232cf38eba5d9c9dbdf488d0ef07dfe
Ryan D <ryandoby10@gmail.com>
2026-07-03 20:50:29 -0400

    Added way for corth to dected numbers

--- src/main.c
+++ src/main.c
@@ -1,20 +1,27 @@
+// Copyright (c) Ryan Doby under The 3-Clause BSD License
+// See the LICENSE file for more info
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib>
 
 int main()
 {
 	bool running = true;
+	// Version should prob be made a char but I made it float so deal with it
 	float version = 0.0;
 
 	char buffer[200];
+	// n is the max number of chars, I have called it n which is a bad var name I know
 	int n = 20;
+	int number;
 
 	printf("Corth Version  %.2f\n", version);
 	printf("Type 'bye' to exit\n");
 
 	while(running == 1)
 	{
+		// Gets rid of the \n in the users input so commands can work
 		fgets(buffer, n, stdin);
 		for(int i = 0; buffer[i] != '\0'; i++)
 		{
@@ -24,11 +31,22 @@ int main()
 				break;
 			}
 		}
+			
+		int stack[5];
+		int top = 0; 
+		if(sscanf(buffer, "%d", &number) == 1)
+		{
+			atoi(buffer);
+		}
 
+		// Exits user when they type bye
 		if (strcmp(buffer, "bye") == 0)
 		{
 			running = 0;
+			printf("Goodbye!\n");
 		} 
+
+
 	}
 	return 0;
 }
